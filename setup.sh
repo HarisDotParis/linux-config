fcd0e092ca47bc6a6d4c0838c317f72ac2a58969
Haris <67912527+HarisDotParis@users.noreply.github.com>
2023-07-05 22:09:25 +0200

    Fixed vim config section in setup.sh

--- setup.sh
+++ setup.sh
@@ -132,7 +132,7 @@ if grep -q "/software-config/vim/.vimrc" ~/.vimrc; then
   echo "${SETUP_SECTION}: already set up."
 else
   echo "${SETUP_SECTION}: setting up..."
-  printf '\nso %s/software-config/vim/.vimrc\n' "${BASEDIR}" >> ~/.zshrc
+  printf '\nso %s/software-config/vim/.vimrc\n' "${BASEDIR}" >> ~/.vimrc
   echo "${SETUP_SECTION}: setup done."
 fi
 
