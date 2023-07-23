10058be22797ef193f07fe177b1256f8c89eac9d
Haris Trgo <haris.trgo.ivz@deutschlandradio.de>
2023-07-23 08:09:17 +0200

    Setup: Added linux-config alias lcupd for git pull of linux-config

--- setup-scripts/linux-config-aliases.sh
+++ setup-scripts/linux-config-aliases.sh
@@ -6,5 +6,6 @@ else
   printf 'alias cdlc="cd %s"\n' "${BASEDIR}" >> ${BASEDIR}/aliases/local_linux_config_aliases
   printf 'alias vimal="vim %s/aliases/generic.sh"\n' "${BASEDIR}" >> ${BASEDIR}/aliases/local_linux_config_aliases
   printf 'alias vimconf="vim %s/software-config/vim/.vimrc"\n' "${BASEDIR}" >> ${BASEDIR}/aliases/local_linux_config_aliases
+  printf 'alias lcupd="git -C %s pull"\n' "${BASEDIR}" >> ${BASEDIR}/aliases/local_linux_config_aliases
   echo "${SETUP_SECTION}: setup done."
 fi
