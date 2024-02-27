95ff17008871eb8cb006601d1cbf1b38bfe56dfa
Haris <67912527+HarisDotParis@users.noreply.github.com>
2024-02-27 04:19:55 +0100

    Update configuration.nix

--- osconfig/nix/configuration.nix
+++ osconfig/nix/configuration.nix
@@ -33,22 +33,22 @@
 
   # Select internationalisation properties.
   i18n = {
-    defaultLocale = "en_IE.utf8";
+    defaultLocale = "en_GB.utf8";
     supportedLocales = [
       "en_GB.UTF-8/UTF-8"
       "en_IE.UTF-8/UTF-8"
       "de_DE.UTF-8/UTF-8"
     ];
     extraLocaleSettings = {
-      LC_ADDRESS = "en_IE.utf8";
-      LC_IDENTIFICATION = "en_IE.utf8";
-      LC_MEASUREMENT = "en_IE.utf8";
+      LC_ADDRESS = "de_DE.utf8";
+      LC_IDENTIFICATION = "en_GB.utf8";
+      LC_MEASUREMENT = "en_GB.utf8";
       LC_MONETARY = "en_IE.utf8";
-      LC_NAME = "en_IE.utf8";
-      LC_NUMERIC = "en_IE.utf8";
-      LC_PAPER = "en_IE.utf8";
-      LC_TELEPHONE = "en_IE.utf8";
-      LC_TIME = "en_IE.utf8";
+      LC_NAME = "en_GB.utf8";
+      LC_NUMERIC = "en_GB.utf8";
+      LC_PAPER = "en_GB.utf8";
+      LC_TELEPHONE = "de_DE.utf8";
+      LC_TIME = "en_GB.utf8";
     };
   };
 
@@ -69,8 +69,11 @@
     xwayland.enable = true;
     sway.enable = true;
     hyprland.enable = true;
+    i3lock.enable = true;
+    i3lock.u2fSupport = true;
   };
   hardware.opengl.enable = true; # necessary for OpenGL support in X11 & for Wayland compositors
+  qt.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -95,18 +98,23 @@
     alsa.enable = true;
     alsa.support32Bit = true;
   };
+  hardware.bluetooth.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  services.flatpak.enable = true;
+  services.fwupd.enable = true;
+
+
   users.defaultUserShell = pkgs.zsh;
+  users.allowNoPasswordLogin = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.haris = {
     isNormalUser = true;
     description = "Haris";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      firefox
       kate
       libreoffice
       obsidian
@@ -122,6 +130,7 @@
       vlc
       yt-dlp
       pipe-viewer # YouTube client with CLI and GUI
+      zoxide
       fzf
       ytfzf
       minitube
@@ -146,7 +155,8 @@
       wsjtx
       vscode.fhs
       viu
-      # flightgear (outdated repo files, also not suitable for very low-spec computers)
+      flightgear
+      libsForQt5.discover
     ];
   };
 
@@ -156,18 +166,181 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   programs = {
-    neovim.enable = true;
-    htop.enable = true;
+    _1password-gui.enable = true;
+    ausweisapp.enable = true;
+    ausweisapp.openFirewall = true;
+    chromium = {
+      enable = true;
+      defaultSearchProviderSuggestURL = "https://www.startpage.com/sp/search?query={searchTerms}";
+      extensions = [
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+        "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1Password
+        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+        "ldpochfccmkkmhdbclfhpagapcfdljkj" # Decentraleyes
+        "cofdbpoegempjloogbagkncekinflcnj" # DeepL Translate
+      ];
+      extraOpts = {
+      # options: https://chromeenterprise.google/intl/en_us/policies/
+        "AutofillAddressEnabled" = false;
+        "AutofillCreditCardEnabled" = false;
+        "AutoplayAllowed" = false;
+        "BackgroundModeEnabled" = false;
+        "BlockThirdPartyCookies" = true;
+        "BrowserSignin" = 0;
+        "BuiltInDnsClientEnabled" = false;
+        "ClearBrowsingDataOnExitList" = [
+          # "browsing_history",
+          "download_history",
+          "cookies_and_other_site_data",
+          "cached_images_and_files",
+          "password_signin",
+          "autofill",
+          # "site_settings",
+          # "hosted_app_data",
+        ];
+        "CloudReportingEnabled" = false;
+        "DefaultBrowserSettingEnabled" = false;
+        "DefaultCookiesSetting" = 4;
+        "DefaultGeolocationSetting" = 2;
+        "DefaultInsecureContentSetting" = 3;
+        "DefaultNotificationsSetting" = 3;
+        "DomainReliabilityAllowed" = false;
+        "ForcedLanguages" = [
+          "en-IE"
+          "en-GB"
+        ];
+        "GoogleSearchSidePanelEnabled" = false;
+        "HttpsOnlyMode" = force_enabled;
+        "NotificationsAllowedForUrls" = [
+          "https://[*.]microsoft.com"
+          "https://[*.]cloud.microsoft"
+        ];
+        "PasswordManagerEnabled" = false;
+        "PaymentMethodQueryEnabled" = false;
+        "RestoreOnStartup" = 1;
+        "SearchSuggestEnabled" = false;
+        "ShowHomeButton" = false;
+        "SideSearchEnabled" = false;
+        "SpellcheckEnabled" = false;
+        "SyncDisabled" = true;
+        "TranslateEnabled" = false;
+        "UserAgentReduction" = 2;
+      };
+    };
+    evolution.enable = true;
+    evolution.plugins = [ pkgs.evolution-ews ];
+    firefox.enable = true;
+    firefox.languagePacks = ["en-GB"];
+    firefox.policies = {
+      "Cookies": {
+        "AcceptThirdParty": "from-visited",
+        "ExpireAtSessionEnd": true,
+        "RejectTracker": true,
+        "Locked": false,
+        "Behavior": "reject-tracker",
+        "BehaviorPrivateBrowsing": "reject-tracker",
+      },
+      "DisablePocket": true,
+      "DisableTelemetry": true,
+      "DisplayMenuBar": "default-on",
+      "DNSOverHTTPS": {
+        "Enabled": false,
+      },
+      "EnableTrackingProtection": {
+        "Value": true,
+        "Locked": false,
+        "Cryptomining": true,
+        "Fingerprinting": true,
+        # "Exceptions": ["https://example.com"]
+      },
+      "ExtensionSettings": {
+        "*": {
+          "blocked_install_message": "Plugin installation not allowed by Nix config policy.",
+          "installation_mode": "blocked",
+          "allowed_types": ["extension", “dictionary”, “locale”]
+        },
+        "uBlock0@raymondhill.net": {
+          "installation_mode": "force_installed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
+        },
+        "addon@darkreader.org": {
+          "installation_mode": "normal_installed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi"
+        },
+        "smartproxy@salarcode.com": {
+          "installation_mode": "normal_installed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/smartproxy/latest.xpi"
+        },
+        "passbolt@passbolt.com": {
+          "installation_mode": "normal_installed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/passbolt/latest.xpi"
+        },
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}": {
+          "installation_mode": "normal_installed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi"
+        },
+        "jid1-BoFifL9Vbdl2zQ@jetpack": {
+          "installation_mode": "normal_installed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/decentraleyes/latest.xpi"
+        },
+        "{da35dad8-f912-4c74-8f64-c4e6e6d62610}": {
+          "installation_mode": "allowed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/auto-refresh-page/latest.xpi"
+        },
+        "{0981817c-71b3-4853-a801-481c90af2e8e}": {
+          "installation_mode": "allowed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/mozlz4-edit/latest.xpi"
+        },
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}": {
+          "installation_mode": "allowed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi"
+        },
+      },
+      "FirefoxHome": {
+        "Search": false,
+        "TopSites": false,
+        "SponsoredTopSites": false,
+        "Highlights": false,
+        "Pocket": false,
+        "SponsoredPocket": false,
+        "Snippets": false,
+        "Locked": false,
+      },
+      "Homepage": {
+        "URL": "about:newtab",
+        "Locked": false,
+        "StartPage": "previous-session",
+      },
+      "NetworkPrediction": true,
+      "NoDefaultBookmarks": true,
+      "OfferToSaveLogins": false,
+      "OverrideFirstRunPage": "",
+      "OverridePostUpdatePage": "",
+      "SearchSuggestEnabled": false,
+      "ShowHomeButton": false,
+    };
     git.enable = true;
-    tmux.enable = true;
-    tmux.clock24 = true;
-    tmux.keyMode = "vi";
+    htop.enable = true;
+    less.enable = true;
+    neovim.enable = true;
+    nm-applet.enable = true;
+    slock.enable = true;
+    ssh.startAgent = true;
+    steam.enable = true;
+    thefuck.enable = true;
+    tmux = {
+      enable = true;
+      clock24 = true;
+      keyMode = "vi";
+      historyLimit = 10000;
+    };
+    traceroute.enable = true;
     vim.defaultEditor = true;
-    # xwayland.enable = true;
     zsh = {
       enable = true;
-      enableAutosuggestions = true;
+      autosuggestions.enable = true;
       enableCompletion = true;
+      histSize = 10000;
       syntaxHighlighting.enable = true;
       ohMyZsh = {
         enable = true;
@@ -193,6 +366,7 @@
           "rust"
           "systemd"
           "terraform"
+          "zoxide"
         ];
       };
     };
@@ -233,7 +407,6 @@
       }
     ];
   };
-  hardware.bluetooth.enable = true;
   fonts = {
     enableDefaultFonts = true;
 #     fontconfig.defaultFonts = { # fontconfig is the font configuration API of X11
